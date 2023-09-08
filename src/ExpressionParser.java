2495f3a9385e48985142652ca7799b546bff4a69
kylecanonigo <kyle.canonigoa@cit.edu>
2023-09-08 11:16:42 +0800

    adding files

--- src/ExpressionParser.java
+++ src/ExpressionParser.java
@@ -1,13 +1,48 @@
+import java.util.Stack;
+
 public class ExpressionParser {
     public static AbstractExpression parseExpression(Context context) {
-        // Implement parsing logic to create the AST here.
-        // For simplicity, let's assume a fixed expression for this example.
-        AbstractExpression number1 = new NumberExpression(5);
-        AbstractExpression number2 = new NumberExpression(3);
-        AbstractExpression addition = new AdditionExpression(number1, number2);
-        AbstractExpression number3 = new NumberExpression(2);
-        AbstractExpression multiplication = new MultiplicationExpression(addition, number3);
-        return multiplication;
+        String postfixExpression = context.getExpression();
+
+        // Tokenize the input expression
+        String[] tokens = postfixExpression.split("\\s+");
+
+        Stack<AbstractExpression> stack = new Stack<>();
+
+        for(String token : tokens) {
+            if (isNumber(token)) {
+                stack.push(new NumberExpression(Integer.parseInt(token)));
+            } else if (isOperator(token)) {
+                // Pop the top two expressions from the stack
+                AbstractExpression right = stack.pop();
+                AbstractExpression left = stack.pop();
+
+                // Create a binary expression based on the operator
+                if (token.equals("+")) {
+                    stack.push(new AdditionExpression(left, right));
+                } else if (token.equals("-")) {
+                    stack.push(new SubtractionExpression(left, right));
+                } else if (token.equals("*")) {
+                    stack.push(new MultiplicationExpression(left, right));
+                }
+            }
+        }
+
+        // The remaining expression on the stack is the result
+        return stack.pop();
+    }
+
+    public static boolean isNumber(String token) {
+        try {
+            Integer.parseInt(token);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isOperator(String token) {
+        return token.matches("[+\\-*/]");
     }
 
     public static void main(String[] args) {
